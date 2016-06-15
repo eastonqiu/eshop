47de2f173a14430ea094b37043ee5283c641791a
qqm <qiuqingmo@lingyunstrong.com>
2016-06-15 15:37:03 +0800

    relocate dir

---
+++ public/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.oc.com/admin/');
+define('HTTP_CATALOG', 'http://www.oc.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.oc.com/admin/');
+define('HTTPS_CATALOG', 'http://www.oc.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/qqm/workspace/oc/public/admin/');
+define('DIR_SYSTEM', '/home/qqm/workspace/oc/public/system/');
+define('DIR_LANGUAGE', '/home/qqm/workspace/oc/public/admin/language/');
+define('DIR_TEMPLATE', '/home/qqm/workspace/oc/public/admin/view/template/');
+define('DIR_CONFIG', '/home/qqm/workspace/oc/public/system/config/');
+define('DIR_IMAGE', '/home/qqm/workspace/oc/public/image/');
+define('DIR_CACHE', '/home/qqm/workspace/oc/public/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/qqm/workspace/oc/public/system/storage/download/');
+define('DIR_LOGS', '/home/qqm/workspace/oc/public/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/qqm/workspace/oc/public/system/storage/modification/');
+define('DIR_UPLOAD', '/home/qqm/workspace/oc/public/system/storage/upload/');
+define('DIR_CATALOG', '/home/qqm/workspace/oc/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'www.oc.com');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ public/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.oc.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.oc.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/qqm/workspace/oc/public/catalog/');
+define('DIR_SYSTEM', '/home/qqm/workspace/oc/public/system/');
+define('DIR_LANGUAGE', '/home/qqm/workspace/oc/public/catalog/language/');
+define('DIR_TEMPLATE', '/home/qqm/workspace/oc/public/catalog/view/theme/');
+define('DIR_CONFIG', '/home/qqm/workspace/oc/public/system/config/');
+define('DIR_IMAGE', '/home/qqm/workspace/oc/public/image/');
+define('DIR_CACHE', '/home/qqm/workspace/oc/public/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/qqm/workspace/oc/public/system/storage/download/');
+define('DIR_LOGS', '/home/qqm/workspace/oc/public/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/qqm/workspace/oc/public/system/storage/modification/');
+define('DIR_UPLOAD', '/home/qqm/workspace/oc/public/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'www.oc.com');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
